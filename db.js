d0dfe0db139f8c22d7794a97455ae64ebe5ef46c
Hardik Raghuvanshi <hardikraghuvanshi@Hardiks-MacBook-Air.local>
2025-12-29 17:44:13 +0530

    string changed

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = ()=>{
-    mongoose.connect('mongodb+srv://hardik:hardik@hardik.tx2wnw3.mongodb.net/crm')
+    mongoose.connect('mongodb+srv://hardik:hardik@hardik.tx2wnw3.mongodb.net/')
     .then((res)=> console.log(`MongoDB Connected Successfully`))
     .catch((err)=> console.log(err))
 }
